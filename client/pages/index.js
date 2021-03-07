84d595252f303bd0ba1b7819808d193c5a64630b
RamsPantoja <worddraco1@gmail.com>
2021-03-07 12:57:05 -0600

    listo para deploy

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Layout from '../components/Layout';
+import Layout from '../components/Layout'
 import styles from './styles/index.module.css';
 import {getSession} from 'next-auth/client';
 import Image from 'next/image';
