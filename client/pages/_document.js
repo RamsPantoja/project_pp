b97f1af68c47eb4a2c2d6d998a4ba85c14781503
RamsPantoja <worddraco1@gmail.com>
2021-03-12 13:13:23 -0600

    Se cambio el favicon de nuevo.

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-            <link rel='icon' type='image/png' href='/favicon.png'/>
+            <link rel='icon' type='image/png' href='/fav.png'/>
         </Head>
         <body>
           <Main />
