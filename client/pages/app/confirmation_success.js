1bb285d5b7c4c62661a85dbf85cdebdb76ce3c5f
RamsPantoja <worddraco1@gmail.com>
2021-03-29 18:15:32 -0600

    Se agrego el signOut() a la pagina confirmation_success, para que cierre la sesion existen y se vea reflejado en cambio en la key isConfirmated el objeto session cuando vuelva a iniciar sesion.

--- client/pages/app/confirmation_success.js
+++ client/pages/app/confirmation_success.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from '../styles/confirmation_success.module.css';
 import Link from 'next/link'
 import { Fragment } from 'react';
 import Head from 'next/head';
+import { signOut } from 'next-auth/client';
 
 const ConfirmationSuccess = () => {
+    useEffect(() => {
+        signOut({redirect:false});
+    }, [])
+
     return (
         <Fragment>
             <Head>
@@ -13,8 +18,8 @@ const ConfirmationSuccess = () => {
             <div className={styles.sucessContainer}>
                 <div className={styles.successCard}>
                     <h2>¡Success!</h2>
-                    <p>Gracias por confirmar tu cuenta de PROFEPACO, haz click aquí para iniciar sesión:</p>
-                    <Link href='/app/signin'><a className={styles.linkToSignIn}>Iniciar sesión</a></Link>
+                    <p>Gracias por confirmar tu cuenta de PROFEPACO, cierra la ventana o haz click aquí para ir a la página de inicio:</p>
+                    <Link href='/'><a className={styles.linkToSignIn}>Ir a inicio</a></Link>
                 </div>
             </div>            
         </Fragment>
