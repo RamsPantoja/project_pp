9fea277bf8c53ef6eeb1a26d9b78fa5189f0e40a
RamsPantoja <worddraco1@gmail.com>
2021-03-29 18:53:40 -0600

    Se corrigio otro bug en confirmation_success con signOut()

--- client/pages/app/confirmation_success.js
+++ client/pages/app/confirmation_success.js
@@ -7,9 +7,11 @@ import { signOut } from 'next-auth/client';
 
 const ConfirmationSuccess = ({token}) => {
 
-    if (token) {
-        signOut({redirect: false})
-    }
+    useEffect(() => {
+        if (token) {
+            signOut({redirect: false})
+        }
+    }, [token, signOut])
 
     return (
         <Fragment>
