fb1bbdcba82b17ffe7ade7feba5c9a34901e3921
RamsPantoja <worddraco1@gmail.com>
2021-05-09 14:50:58 -0500

    Se cambio el notification_url de la preferencia de mercado pago, para llevar a produccion.

--- client/lib/resolvers.js
+++ client/lib/resolvers.js
@@ -411,7 +411,7 @@ export const resolvers = {
                 payment_methods: {
                     installments: 1
                 },
-                notification_url: 'https://profepaco-git-feature-ipnnotifications-ramspantoja.vercel.app/api/ipn',
+                notification_url: 'https://profepaco.com/api/ipn',
                 statement_descriptor: 'PROFEPACO',
                 back_urls: {
                     success: "https://profepaco.com/success_payment"
